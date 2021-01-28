800d54f75e6a570beb8be8d631181980fc6aeefa
深澤　寛太 <kantafkzw@gmail.com>
2021-01-28 09:59:45 +0900

    profileページ基本設定

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,5 +3,26 @@ class ProfilesController < ApplicationController
 
   def show
     @user = current_user
+    @profile = current_user.profile
+    @profile = current_user.build_profile
   end
+
+  # def edit
+  #   @profile = current_user.build_profile
+  # end
+
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィール更新しました。'
+    else
+      flash.now[:error] = '更新できませんでした。'
+      render :edit
+    end
+  end
+
+  private
+    def profile_params
+      params.require(:profile).permit(:nickname, :avatar)
+    end
 end
