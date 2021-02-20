4826c2c10527067fd8e8a60c90ae42f280dc2386
深澤　寛太 <kantafkzw@gmail.com>
2021-02-21 08:08:32 +0900

    ajax form編集 nickname更新表示処理　途中③

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,7 +2,6 @@ class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
     @profile = current_user.profile
   end
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,28 +31,37 @@ document.addEventListener(
       document.getElementById("submit-btn").onclick = function () {
         $('#overlay').fadeOut();
       };
-    };
+    }
+
+    $("#profile-form").on("#submit-btn", function(e) {
+        e.preventDefault();
+        const nickname = $("#nickname-form").val();
+        const avatar = $("#yourimg-form").val();
+        const dataset = $("#profile_container").data();
+        const profileId = dataset.profileId
+        
+        $.ajax({
+          url: `/profile/{profileId}`,
+          type: "PUT",
+          data: {
+            profile: {
+              nickname:nickname,
+              avatar:avatar
+            }
+          },
+          dataType: "json"
+        })
+      })
+
+    axios.get(`/profile/${profileId}`)
+      .then((response) => {
+        debugger
+        const profile = response.data
+
+        $(".your_name").append(`<p><%=@profile.nickname=></p>`)
+        $(".your_img").append(`<p><%=@profile.avatar=></p>`)
+      })
   },
   
-  $("#profile-form").on("#submit-btn", function(e) {
-    e.preventDefault();
-    const nickname = $("#nickname-form").val();
-    const avatar = $("#yourimg-form").val()
-    
-    $.ajax({
-      url: "/profile/",
-      type: "PUT",
-      data: {
-        profile: {
-          nickname:nickname,
-          avatar:avatar
-        }
-      },
-      dataType: "json"
-    })
   
-    .done(function(data) {
-      $(".your_name").append("<%=@profile.nickname%>")
-    })
-  })
 );
