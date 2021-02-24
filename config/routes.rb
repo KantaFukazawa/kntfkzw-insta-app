53dc85827da1d57824aa4201da33f1d7ba3b9d3a
深澤　寛太 <kantafkzw@gmail.com>
2021-02-24 10:25:37 +0900

    postモデル、ルーティング、コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts
 
   resource :profile, only: [:show, :edit, :update]
 end
