a827c9654d92a986683717fb5ce3808f8aa99d37
深澤　寛太 <kantafkzw@gmail.com>
2021-02-27 11:04:14 +0900

    post　view、コントローラー(仮)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
 class PostsController < ApplicationController
   def index
-    
   end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:content, :image)
+    end
 end
