82944cd33e3653154395333ce37260a16effbab5
深澤　寛太 <kantafkzw@gmail.com>
2021-03-13 09:16:27 +0900

    routesの設定。likeのON、OFF切替可。

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  def has_liked?(post)
+    likes.exists?(post_id: post.id)
+  end
 end
